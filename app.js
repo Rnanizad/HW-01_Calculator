90a68e49773da70bec6069b785871f25b2b6025e
Rnanizad <rasool.6n6@gmail.com>
2019-09-26 21:35:23 +0330

    upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+function sum(a, b){
+    a + b
+
+    return c = a + b
+}
+
+ 
+function minus(a, b){
+     a - b
+
+     return c = a - b
+ }
+
+function zabdar(a, b){
+     a * b
+
+     return c = a * b
+ }
+
+ function taghsim (a, b){
+     a / b
+
+    return c = a / b
+ }
